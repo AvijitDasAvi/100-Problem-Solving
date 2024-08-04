cdfdb5790b3d0aad0990c03e5410adf129fd6705
Avijit Das <avijitavi338895@gmail.com>
2024-08-05 00:20:46 +0600

    Problem 14

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,3 +1,33 @@
-public class LinearSearch {
+import java.util.Scanner;
 
+public class LinearSearch {
+    int length;
+
+    public int linearSearch(int[] array, int target) {
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] array = { 3, 5, 7, 9, 11, 13, 15 };
+
+        System.out.print("Enter the target: ");
+        int target = input.nextInt();
+
+        LinearSearch obj = new LinearSearch();
+        int result = obj.linearSearch(array, target);
+        if (result != -1) {
+            System.out.println("The target found at position " + (result + 1));
+        } else {
+            System.out.println("The target is not found.");
+        }
+        input.close();
+    }
 }
