a4e3f4413363084eb2468e7226d37543a00d3bc5
Avijit Das <avijitavi338895@gmail.com>
2024-08-05 03:26:36 +0600

    Problem 15

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,3 +1,27 @@
-public class ReverseString {
+import java.util.Scanner;
 
+public class ReverseString {
+    public String reverse(String str) {
+        String result = " ";
+        char temp;
+        for (int i = 0; i < str.length(); i++) {
+            temp = str.charAt(i);
+            result = temp + result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String str = input.nextLine();
+
+        ReverseString obj = new ReverseString();
+        String result = obj.reverse(str);
+
+        System.out.println("The reverse output: " + result);
+
+        input.close();
+    }
 }
