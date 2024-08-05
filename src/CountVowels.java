0bbf1752acf0f142cad2a737649a3f9b30fc2743
Avijit Das <avijitavi338895@gmail.com>
2024-08-05 11:07:44 +0600

    Problem 16

--- src/CountVowels.java
+++ src/CountVowels.java
@@ -1,3 +1,34 @@
-public class CountVowels {
+import java.util.Scanner;
 
+public class CountVowels {
+    public int counting(String input) {
+        int count = 0;
+        input = input.toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o'
+                    || input.charAt(i) == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        CountVowels countVowels = new CountVowels();
+
+        System.out.print("Enter the word: ");
+        String input = str.nextLine();
+
+        int result = countVowels.counting(input);
+
+        if (result == 0) {
+            System.out.println("No vowels are found.");
+        } else {
+            System.out.println("Total number of vowels are - " + result);
+        }
+
+        str.close();
+
+    }
 }
