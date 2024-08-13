ac9313fa56da9588e88f41996285d692ee8f954a
Avijit Das <avijitavi338895@gmail.com>
2024-08-13 23:27:30 +0600

    Problem 20

---
+++ src/MatrixMultiplication.java
@@ -0,0 +1,8 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+
+        int[] array = { 2, 4, 6, 3, 6, 2, 55, 7, 53, 6, 3 };
+        int length = array.length;
+        System.out.println(length);
+    }
+}
