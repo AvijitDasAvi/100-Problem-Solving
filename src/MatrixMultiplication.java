2b619effc14a9a29de0820761b1fafe827331869
Avijit Das <avijitavi338895@gmail.com>
2024-08-15 00:01:05 +0600

    Problem 20

--- src/MatrixMultiplication.java
+++ src/MatrixMultiplication.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class MatrixMultiplication {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        int[] array = { 2, 4, 6, 3, 6, 2, 55, 7, 53, 6, 3 };
-        int length = array.length;
-        System.out.println(length);
+        int[][] matrixA = new int[2][3];
+        int[][] matrixB = new int[3][3];
+        int[][] result = new int[2][3];
+
+        System.out.print("Enter the elements in matrix: ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrixA[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("The matrix-A is: ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrixA[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
